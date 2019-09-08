d87287ffd8d4abf596021b7f1b9430d0e6cef0b0
kiwsan <kiwsanthia@gmail.com>
2019-09-08 12:23:18 +0700

    refresh token updated

--- IdentityWebApp/Providers/RefreshTokenProvider.cs
+++ IdentityWebApp/Providers/RefreshTokenProvider.cs
@@ -19,18 +19,7 @@ namespace IdentityWebApp.Providers
 
             string refreshTokenId = Guid.NewGuid().ToString("n");
 
-            /*
-            context.Ticket.Properties.IssuedUtc = token.IssuedUtc;
-            context.Ticket.Properties.ExpiresUtc = token.ExpiresUtc;
-
-            token.ProtectedTicket = context.SerializeTicket();
-
-            var result = authRepository.AddRefreshToken(token);
-
-            if (result)
-            {
-                context.SetToken(refreshTokenId);
-            }*/
+            //set new refresh token
 
             return Task.FromResult<object>(null);
         }
@@ -45,14 +34,7 @@ namespace IdentityWebApp.Providers
 
             context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
 
-            /*             
-                if (refreshToken != null)
-                {
-                    context.DeserializeTicket(refreshToken.ProtectedTicket);
-
-                    authRepository.RemoveRefreshToken(hashedTokenId);
-                }
-             */
+            //remove refresh token
 
             return Task.FromResult<object>(null);
         }
